f470c8cb827bd15c484f9adc623104352f14deb1
Sachin Sharma <169078045+chinxcode@users.noreply.github.com>
2024-09-19 23:01:55 +0530

    Update competitionRoutes.js

--- src/routes/competitionRoutes.js
+++ src/routes/competitionRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const competitionController = require("../controllers/competitionController");
 
-router.post("/update-competition-data", competitionController.updateCompetitionData);
+router.get("/update-competition-data", competitionController.updateCompetitionData);
 
 router.get("/leaderboard", competitionController.getLeaderboard);
 
