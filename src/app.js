e24fd2d6a92fd946015b634a6f11c302e7950581
sachin <sachinsharma11175@gmail.com>
2024-09-19 21:12:38 +0530

    Refactor package-lock.json and package.json to add cors dependency

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const competitionRoutes = require("./routes/competitionRoutes");
 const { connectToDatabase } = require("./config/database");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
 app.use(express.json());
 
